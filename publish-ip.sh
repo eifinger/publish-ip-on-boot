0d57c4d1fe769a023b64951cf265ca7835693963
Kevin Eifinger <k.eifinger@googlemail.com>
2019-05-14 12:27:13 +0200

    Update publish-ip.sh

--- publish-ip.sh
+++ publish-ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ip=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $4 }')
+ip=$(ifconfig wlan0 | grep "inet " | awk -F'[: ]+' '{ print $3 }')
 mosquitto_pub -t "pibakery-eifinger/ip" -h "broker.hivemq.com" -m "$ip"
